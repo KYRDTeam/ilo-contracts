c821b671bb5c9be46c122173f3f384ce7950f2da
haupc <haupc.uet@gmail.com>
2024-05-31 17:46:20 +0700

    fix: some internal audit issue and comment out functions

--- src/base/ILOVest.sol
+++ src/base/ILOVest.sol
@@ -16,11 +16,15 @@ abstract contract ILOVest is IILOConfig {
 
     function _unlockedLiquidity(uint256 tokenId) internal view virtual returns (uint128 unlockedLiquidity);
 
+    /// @notice return number of sharesBPS unlocked upto now
     function _unlockedSharesBPS(LinearVest[] storage vestingSchedule) internal view returns (uint16 unlockedSharesBPS) {
         for (uint256 index = 0; index < vestingSchedule.length; index++) {
 
             LinearVest storage vest = vestingSchedule[index];
 
+            // if vest already end, all the shares are unlocked
+            // otherwise we calculate percentage of unlocked times and get the unlocked share number
+            // all vest after current unlocking vest is ignored
             if (vest.end < block.timestamp) {
                 unlockedSharesBPS += vest.percentage;
             } else if(vest.start < block.timestamp && block.timestamp < vest.end ) {
